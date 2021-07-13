c71997cbd95628711beb509b98a0ad2591a77517
Rison <2462301753@qq.com>
2021-07-13 19:30:15 +0800

    [feat] 将dataFrame数据保存到HBase表中

--- tags-model/src/main/scala/com/rison/tag/tools/HBaseTools.scala
+++ tags-model/src/main/scala/com/rison/tag/tools/HBaseTools.scala
@@ -1,9 +1,9 @@
 package com.rison.tag.tools
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.client.{Result, Scan}
+import org.apache.hadoop.hbase.client.{Put, Result, Scan}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.mapreduce.{TableInputFormat, TableOutputFormat}
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil
 import org.apache.hadoop.hbase.util.{Base64, Bytes}
 import org.apache.spark.rdd.RDD
@@ -17,13 +17,14 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
  */
 object HBaseTools {
   /**
-   * 读取Hbase表数据
-   * @param spark
-   * @param zks
-   * @param port
-   * @param table
-   * @param family
-   * @param fields
+   * 依据指定表名称、列簇及列名称从HBase表中读取数据
+   *
+   * @param spark  SparkSession 实例对象
+   * @param zks    Zookerper 集群地址
+   * @param port   Zookeeper端口号
+   * @param table  HBase表的名称
+   * @param family 列簇名称
+   * @param fields 列名称
    * @return
    */
   def read(spark: SparkSession, zks: String, port: String, table: String, family: String, fields: Seq[String]): DataFrame = {
@@ -85,15 +86,61 @@ object HBaseTools {
   }
 
   /**
-   * 将dataFrame数据保存到HBase表中
-   * @param dataframe
-   * @param zks
-   * @param port
-   * @param table
-   * @param family
-   * @param rowKeyColumn
+   * 将DataFrame数据保存到HBase表中
+   *
+   * @param dataframe    数据集DataFrame
+   * @param zks          Zk地址
+   * @param port         端口号
+   * @param table        表的名称
+   * @param family       列簇名称
+   * @param rowKeyColumn RowKey字段名称
    */
-  def write(dataframe: DataFrame, zks: String, port: String, table: String, family: String, rowKeyColumn: String): Unit ={
-
+  def write(dataframe: DataFrame, zks: String, port: String, table: String, family: String, rowKeyColumn: String): Unit = {
+    /*
+        def saveAsNewAPIHadoopFile(
+        主类测试代码如下：
+        path: String,
+        keyClass: Class[_],
+        valueClass: Class[_],
+        outputFormatClass: Class[_ <: NewOutputFormat[_, _]],
+        conf: Configuration = self.context.hadoopConfiguration
+        ): Unit
+     */
+    // 1 设置HBase中zookeeper集群信息
+    val config = new Configuration()
+    config.set("hbase.zookeeper.quorum", zks)
+    config.set("hbase.zookeeper.property.clientPort", port.toString)
+    //2 设置读写HBase表的名称
+    config.set(TableOutputFormat.OUTPUT_TABLE, table)
+    //3 数据转换
+    val columns: Array[String] = dataframe.columns
+    val putsRDD: RDD[(ImmutableBytesWritable, Put)] = dataframe.rdd
+      .map {
+        row =>
+          //获取rowKey
+          val rowKey: String = row.getAs[String](rowKeyColumn)
+          //构建Put对象
+          val put = new Put(Bytes.toBytes(rowKey))
+          //将每列数据加入到Put对象中
+          val familyBytes: Array[Byte] = Bytes.toBytes(family)
+          columns.foreach {
+            column =>
+              put.addColumn(
+                familyBytes,
+                Bytes.toBytes(column),
+                Bytes.toBytes(row.getAs[String](column))
+              )
+          }
+          //返回二元组
+          (new ImmutableBytesWritable(put.getRow), put)
+      }
+    // 保存数据到表
+    putsRDD.saveAsNewAPIHadoopFile(
+      s"/apps/hbase/$table-" + System.currentTimeMillis(),
+      classOf[ImmutableBytesWritable],
+      classOf[Put],
+      classOf[TableOutputFormat[ImmutableBytesWritable]],
+      config
+    )
   }
 }
