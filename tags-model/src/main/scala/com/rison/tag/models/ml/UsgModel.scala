9f2dc9d6dc894f758104c7cea8be5bdeff143681
Rison <2462301753@qq.com>
2021-08-16 14:57:17 +0800

    [feat] 用户购物性别USG模型

--- tags-model/src/main/scala/com/rison/tag/models/ml/UsgModel.scala
+++ tags-model/src/main/scala/com/rison/tag/models/ml/UsgModel.scala
@@ -1,6 +1,11 @@
 package com.rison.tag.models.ml
 
 import com.rison.tag.models.{AbstractModel, ModelType}
+import com.rison.tag.tools.TagTools
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{VectorAssembler, VectorIndexer}
+import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 /**
@@ -8,12 +13,7 @@ import org.apache.spark.sql.{Column, DataFrame, SparkSession}
  *         挖掘类型标签:用户购物性别USG模型
  */
 class UsgModel extends AbstractModel("用户购物性别USG模型", ModelType.ML) {
-  /**
-   * 380 用户购物性别
-   * 381 男 0
-   * 382 女 1
-   * 383 中性 -1
-   */
+
   /**
    *
    * @param businessDF
@@ -120,12 +120,129 @@ class UsgModel extends AbstractModel("用户购物性别USG模型", ModelType.ML
       .select(
         $"memberid".as("userId"), $"color", $"product", $"label"
       )
+    //组合特征值及转换特征
+    val featuresDF: DataFrame = featuresTransForm(genderDF)
+    //5 加载决策数模型
+    val dtcModel: DecisionTreeClassificationModel = trainModel(featuresDF)
+    val predictionDF: DataFrame = dtcModel.transform(featuresDF)
+    //6 统计每个会员购买商品性别次数，最终确定用户的购物性别
+    val usgDF: DataFrame = predictionDF
+      .select(
+        $"userId",
+        //当prediction 为0 时， 表示为男性商品
+        when($"prediction" === 0, 1)
+          .otherwise(0).as("male"), //男
+        //当prediction为1时，表示女性商品
+        when($"prediction" === 1, 1)
+          .otherwise(0).as("female") // 女
+      )
+      .groupBy($"userId")
+      .agg(
+        count($"userId").as("total"), //用户购物次数
+        sum($"male").as("maleTotal"), //预测为男性的次数
+        sum($"female").as("femaleTotal") //预测为女性的次数
+      )
 
+    //7 属性标签数据中规则rule
+    val rulesMap: Map[String, String] = TagTools.convertMap(tagDF)
 
+    //8 自定义UDF函数
+    /**
+     * 计算每个用户近半年内所有的订单中的男性商品超过60%则认为该用户为男，或者近半年内所有的订单中的女性商品超过60%则认定用户为女性
+     */
+    val gender_tag_udf: UserDefinedFunction = udf(
+      (total: Long, maleCount: Double, femaleCount: Double) => {
+        val maleRate: Double = maleCount / total
+        val femaleRate: Double = femaleCount / total
+        if (maleCount >= 0.6) {
+          rulesMap("0")
+        } else if (femaleRate >= 0.6) {
+          rulesMap("1")
+        } else {
+          rulesMap("-1")
+        }
+      }
+    )
+    //9 根据该用户的total/maleCount/femaleCount判断用户应该被打上那个标签
+    val modelDF: DataFrame = usgDF.select(
+      $"userId",
+      gender_tag_udf($"total", $"maleTotal", $"femaleTotal").as("usg")
+    )
+    modelDF
+  }
 
+  /**
+   * 针对数据集进行特征工程：特征提取、特征转换及特征选择
+   *
+   * @param dataframe 数据集
+   * @return 数据集，包含特征列features: Vector类型和标签列label
+   */
+  def featuresTransForm(dataframe: DataFrame): DataFrame = {
+    // 特征向量化
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("color", "product"))
+      .setOutputCol("raw_features")
+    val df1: DataFrame = assembler.transform(dataframe)
+    //类别特征进行索引
+    val vectorIndexer = new VectorIndexer()
+      .setInputCol("raw_features")
+      .setOutputCol("features")
+      .setMaxCategories(30)
+      .fit(df1)
+    val df2: DataFrame = vectorIndexer.transform(df1)
+    df2
+  }
 
+  /**
+   * 模型评估，返回计算分类指标值
+   *
+   * @param dataframe  预测结果的数据集
+   * @param metricName 分类评估指标名称，支持：f1、weightedPrecision、
+   *                   weightedRecall、accuracy
+   * @return
+   */
+  def modelEvaluate(dataframe: DataFrame, metricName: String) = {
+    //构建多分类类器
+    val evaluator: MulticlassClassificationEvaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      //指标名称
+      .setMetricName(metricName)
+    val metric: Double = evaluator.evaluate(dataframe)
+    metric
+  }
 
+  /**
+   * 使用决策树分类算法训练模型，返回DecisionTreeClassificationModel模型
+   *
+   * @return
+   */
+  def trainModel(featuresDF: DataFrame) = {
+    // 数据划分为训练集和测试集合
+    val Array(trainingDF, testingDF) = featuresDF.randomSplit(Array(0.8, 0.2), seed = 123)
+    //构建决策树分类器
+    val dtc: DecisionTreeClassifier = new DecisionTreeClassifier()
+      .setFeaturesCol("features")
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMaxDepth(5) // 树的深度
+      .setMaxBins(32) //树的叶子数目
+      .setImpurity("gini") //基尼系数
 
-    null
+    //训练模型
+    logWarning("正在训练模型...")
+    val dtcModel: DecisionTreeClassificationModel = dtc.fit(trainingDF)
+    val predictionDF: DataFrame = dtcModel.transform(testingDF)
+    println(s"accuracy = ${modelEvaluate(predictionDF, "accuracy")}")
+    dtcModel
+
+  }
+
+}
+
+object UsgModel {
+  def main(args: Array[String]): Unit = {
+    val tagModel = new UsgModel()
+    tagModel.executeModel(380L)
   }
 }
